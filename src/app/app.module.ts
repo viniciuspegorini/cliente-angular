243d535a92c3ba84b4d69c404da3eabec1c7b9b4
Vinicius Pegorini <viniciuspegorini@gmail.com>
2018-09-18 22:11:10 -0300

    aula 18/09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { IndexComponent } from './index/index.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { GeneroComponent } from './genero/genero.component';
+import { GeneroService } from './genero/genero.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    IndexComponent,
+    GeneroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    GeneroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/genero/genero.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { GeneroService } from './genero.service';
+import { Genero } from '../model/genero';
+
+@Component({
+  selector: 'app-genero',
+  templateUrl: './genero.component.html',
+  styleUrls: ['./genero.component.css']
+})
+export class GeneroComponent implements OnInit {
+
+  generos: Genero[];
+  teste: string = '1234';
+
+  constructor(private generoService: GeneroService) { }
+
+  ngOnInit() {
+    this.findAll();
+  }
+
+  findAll() {
+    this.generoService.findAll().subscribe(
+                    e => this.generos = e);
+  }
+
+}
