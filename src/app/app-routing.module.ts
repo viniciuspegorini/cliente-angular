4d316bc9f2e3fd199ab8060cb325b306e89c3bf6
Vinicius Pegorini <viniciuspegorini@gmail.com>
2018-10-16 21:07:25 -0300

    aula 16/10-21h07

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,11 @@ import {Routes, RouterModule} from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { GeneroComponent } from './genero/genero.component';
 import { ProdutoraComponent } from './produtora/produtora.component';
-import { SerieComponent } from './serie/serie.component';
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'genero', component: GeneroComponent},
-  {path: 'produtora', component: ProdutoraComponent},
-  {path: 'serie', component: SerieComponent}
+  {path: 'produtora', component: ProdutoraComponent}
 ];
 
 @NgModule({
